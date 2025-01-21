7d51436f70502e46c40afbb44dc90b08783653ec
jaaysus <issaelkaddaoui@gmail.com>
2025-01-21 19:37:55 +0000

    bruh

--- src/components/NormalHome.jsx
+++ src/components/NormalHome.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FlowersGallery from './FlowersGallery';
 import Hero from './Hero';
 
 const NormalUserHome = () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = '#07202B';
+    return () => {
+      document.body.style.backgroundColor = ''; // Reset when the component is unmounted
+    };
+  }, []);
+
   return (
     <>
       <Hero />
