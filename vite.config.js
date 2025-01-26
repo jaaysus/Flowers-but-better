19fe6716121ffe7242a798a0d45c6d9d88222c51
jaaysus <issaelkaddaoui@gmail.com>
2025-01-26 12:39:08 +0000

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Replace with your repo name
   plugins: [react()],
+  base: '/Flowers-but-better/',
 });
+
