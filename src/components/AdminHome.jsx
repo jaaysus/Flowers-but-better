cc8eeedfe27a21ed7e2d879c21a6ea78bfc81551
amalfertoul <142597174+amalfertoul@users.noreply.github.com>
2025-01-26 12:37:25 +0100

    update

--- src/components/AdminHome.jsx
+++ src/components/AdminHome.jsx
@@ -1,23 +1,46 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { logout } from '../redux/actions'; // Assuming you have this import
+
 
 const AdminHome = () => {
     const currentUser = useSelector((state) => state.users.currentUser);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    
 
     useEffect(() => {
         if (currentUser) {
             if (!currentUser.isAdmin) {
                 navigate('/userHome');
             }
+            else if (currentUser.isAdmin) {
+                navigate('/adminHome');
+            }
         } else {
             navigate('/userHome'); 
         }
     }, [currentUser, navigate]);
 
+    const handleLogout = () => {
+            dispatch(logout());  // Dispatch logout action
+            navigate('/userHome'); // Redirect to userHome after logout
+        };
+    
+
     if (currentUser && currentUser.isAdmin) {
-        return <h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome Admin</h1>;
+        return (
+            <>
+                <h1 style={{ textAlign: 'center', marginTop: '50px' }}>
+                    Welcome Admin
+                    <button onClick={handleLogout}>
+                        Logout
+                    </button>
+                    </h1>
+                
+        </>
+);
     }
 
     return null; 
