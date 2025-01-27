47b493ddfe29b78530f9248c3ecf6e118f949004
jaaysus <issaelkaddaoui@gmail.com>
2025-01-27 22:26:20 +0000

    fixed Account info bug

--- src/containers/Account.jsx
+++ src/containers/Account.jsx
@@ -13,14 +13,14 @@ export default function Account() {
     const currentUser = useSelector((state) => state.users.currentUser); 
     const orderInfo = useSelector((state) => state.panier.orderInfo);
 
-
     const [isEditing, setIsEditing] = useState(false);
     const [editedUser, setEditedUser] = useState({
+        ...currentUser, // fixed edit bug
         username: currentUser.username,
         email: currentUser.email,
+        fullName: currentUser.fullName, // Add fullName here
     });
 
-    
     useEffect(() => {
         if (currentUser) {
             if (currentUser.isAdmin) {
@@ -38,14 +38,12 @@ export default function Account() {
         navigate('/Flowers-but-better/userHome'); // Redirect to userHome after logout
     };
 
-
     const handleEditClick = () => {
         setIsEditing(true); // Set edit mode to true
     };
 
     const handleSaveClick = () => {
-        dispatch(updateUserInfo(editedUser));
-
+        dispatch(updateUserInfo(editedUser)); // Pass the updated user object
         setIsEditing(false); // Exit edit mode
     };
 
@@ -58,79 +56,90 @@ export default function Account() {
 
     return (
         <>            
-        <h1 className="acc-title">Profile</h1>
-        
+            <h1 className="acc-title">Profile</h1>
 
-        <div id='account-container'>
-            
-            
-            <section className="personal-info">
-                <h2>Personal Info 
-                <button className="logout-button" onClick={handleLogout}>
-                Logout
-                </button>
-                <span className="edit-link" onClick={handleEditClick}>(Edit)</span></h2>
-                {isEditing ? (
-                    <div>
-                        <div>
-                            <strong>Username:</strong>
-                            <input
-                                type="text"
-                                name="username"
-                                value={editedUser.username}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div>
-                            <strong>Email:</strong>
-                            <input
-                                type="email"
-                                name="email"
-                                value={editedUser.email}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <button onClick={handleSaveClick}>Save</button>
-                    </div>
-                ) : (
-                    <div>
-                        <div><strong>Username:</strong> {currentUser.username}</div>
-                        <div><strong>Email:</strong> {currentUser.email}</div>
-                    </div>
-                )}
-            </section>
-            <section className="order-history">
-                {orderInfo.order && orderInfo.order.length > 0 ? (
-                    <div>
-                        <h2>Order History</h2>
-                        <ul className="order-list">
-                            {orderInfo.order.map((order, index) => (
-                                <li key={index} className="order-item">
-                                    <strong>Tracking Number: {order.trackingNumber}</strong>
-                                    <br />
-                                    <p>{order.date}</p> 
+            <div id='account-container'>
+                <section className="personal-info">
+                    <h2>Personal Info 
+                    <button className="logout-button" onClick={handleLogout}>
+                        Logout
+                    </button>
+                    <span className="edit-link" onClick={handleEditClick}>(Edit)</span></h2>
 
-                                    <ul className="order-details">
-                                        {order.items.map((item, itemIndex) => (
-                                            <li key={itemIndex}>
-                                                {item.nom} - <strong>Quantity:</strong> {item.quantite} - <strong>Price:</strong> {item.prix}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ) : (
-                    <p className="no-orders">No orders found</p>
-                )}
-            </section>
-                    <FeedbackForm />
-            <section className="calendar-booking">
-                <h2>Calendar and Booking</h2>
-                <Calendar />
-            </section>
-        </div>
+                    {isEditing ? (
+                        <div>
+                            <div>
+                                <strong>Full Name:</strong>
+                                <input
+                                    type="text"
+                                    name="fullName"
+                                    value={editedUser.fullName}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <strong>Username:</strong>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    value={editedUser.username}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <strong>Email:</strong>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={editedUser.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <button onClick={handleSaveClick}>Save</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <div><strong>Full Name:</strong> {currentUser.fullName}</div>
+                            <div><strong>Username:</strong> {currentUser.username}</div>
+                            <div><strong>Email:</strong> {currentUser.email}</div>
+                        </div>
+                    )}
+                </section>
+
+                <section className="order-history">
+                    {orderInfo.order && orderInfo.order.length > 0 ? (
+                        <div>
+                            <h2>Order History</h2>
+                            <ul className="order-list">
+                                {orderInfo.order.map((order, index) => (
+                                    <li key={index} className="order-item">
+                                        <strong>Tracking Number: {order.trackingNumber}</strong>
+                                        <br />
+                                        <p>{order.date}</p> 
+
+                                        <ul className="order-details">
+                                            {order.items.map((item, itemIndex) => (
+                                                <li key={itemIndex}>
+                                                    {item.nom} - <strong>Quantity:</strong> {item.quantite} - <strong>Price:</strong> {item.prix}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ) : (
+                        <p className="no-orders">No orders found</p>
+                    )}
+                </section>
+
+                <FeedbackForm />
+                
+                <section className="calendar-booking">
+                    <h2>Calendar and Booking</h2>
+                    <Calendar />
+                </section>
+            </div>
         </>
     );
 }
