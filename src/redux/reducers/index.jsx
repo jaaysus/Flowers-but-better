21cba713074fc64860b5d498095d73558d89118d
jaaysus <issaelkaddaoui@gmail.com>
2025-01-28 01:51:18 +0000

    CalendarReducer to CalendarSlice

--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -4,7 +4,7 @@ import productReducer from './productReducer';
 import panierReducer from './panierReducer';
 import cardReducer from './cardReducer';
 import reviewsReducer from './reviewsReducer'; 
-//import calendarReducer from "./calendarReducer";
+//import calendarReducer from "./calendarReducer"; this can be removed too
 import CalendarSlice from '../slices/CalendarSlice';
 const Rootreducer = combineReducers({
   users: userReducer,
