a395bf86e656281f24d77d8219f9840486525e62
Issa El Kaddaoui <issaelkaddaoui@gmail.com>
2025-10-24 14:23:59 +0100

    blank webpage  error is fixed

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
-import { thunk } from "redux-thunk";
 
 const store = configureStore({//first step to morph reducers into toolkit
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // Add thunk middleware
 });
 
 export default store;
